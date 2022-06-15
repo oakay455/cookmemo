b501762d42fd7894f3074e83d0a785864b6c169d
oakay455 <ayako110.89@gmail.com>
2022-06-15 12:15:45 +0000

    fix-members-db

--- db/migrate/20220607045344_devise_create_members.rb
+++ db/migrate/20220607045344_devise_create_members.rb
@@ -7,7 +7,7 @@ class DeviseCreateMembers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false, default: ""
-      t.text :introduction, null: false, default: ""
+      t.text :introduction
       t.boolean :is_deleted, null: false, default: "FALSE"
 
       ## Recoverable
