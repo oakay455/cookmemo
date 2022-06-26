a2bd78b114161e36c3233d187f357fee2bd8cb78
oakay455 <ayako110.89@gmail.com>
2022-06-26 02:47:07 +0000

    content_type記述変更

--- app/models/member.rb
+++ app/models/member.rb
@@ -23,7 +23,7 @@ class Member < ApplicationRecord
   # has_many :bookmarked_recipes, through: :bookmarks, source: :recipe
 
   has_one_attached :profile_image
-  
+
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |member|
       member.password = SecureRandom.urlsafe_base64
@@ -34,7 +34,7 @@ class Member < ApplicationRecord
   def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/no_profile_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
   end
   profile_image.variant(resize_to_limit: [width, height]).processed
   end
@@ -81,5 +81,5 @@ class Member < ApplicationRecord
   def favorited_by?(recipe_id)
     recipe_favorites.where(recipe_id: recipe_id).exists?
   end
-  
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -20,7 +20,7 @@ class Recipe < ApplicationRecord
   def get_recipe_image(width, height)
     unless recipe_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      recipe_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      recipe_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     recipe_image.variant(resize_to_limit: [width, height]).processed
   end
